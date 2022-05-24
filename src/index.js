aa89149325169ba03b2b5db4d705f3556987a493
Dhaval <umaretiyadhaval123@gmail.com>
2022-05-24 11:32:32 +0530

    start

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const  mongoose  = require('mongoose');
+const app = express();
+
+const multer= require("multer");
+const { AppConfig } = require('aws-sdk');
+app.use(bodyParser.json());
+
+app.use(multer().any())
+
+mongoose.connect("mongodb+srv://dhaval-functionup:gRrFSwHIong4NV9B@cluster0.az0cl.mongodb.net/pro-5(group-27)?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
